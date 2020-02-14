3134fe83f5243bbb3954b971ccc4e29b631163c1
crazyyyboy <jen1ka228@gmail.com>
2020-02-14 23:20:37 +0300

    added slider-2

--- app/js/main.js
+++ app/js/main.js
@@ -18,6 +18,15 @@ $(function(){
       nextArrow: '<button class="slick-next slick-arrow"><img src="images/right.png" alt=""></button>',
     });
 
+    $('.slider-2__inner').slick({
+      dots: false,
+      arrows: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      prevArrow: '<button class="slick-prev slick-arrow"><img src="images/left.png" alt=""></button>',
+      nextArrow: '<button class="slick-next slick-arrow"><img src="images/right.png" alt=""></button>',
+    });
+
     var mixer = mixitup('.product__inner-box');
 
 });
